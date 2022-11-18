a1ab6fb1717b1b910f79c82d9b0dd3e8abf372a0
CHARLES-HENRI PRAT <charprat@yahoo.fr>
2022-11-18 16:46:30 +0100

    [infrastructure] add labels v2 to train/test dataset

--- dissidentia/infrastructure/dataset.py
+++ dissidentia/infrastructure/dataset.py
@@ -1,7 +1,11 @@
 """Load train dataset for dissident detection """
 
 import os
+import logging
 import pandas as pd
+
+from sklearn.model_selection import train_test_split
+
 from dissidentia.infrastructure.grand_debat import get_rootdir
 
 
@@ -14,10 +18,23 @@ def get_train_test_split():
         y_train (Series)
         y_test (Series)
     """
-    dataset_path = os.path.join(get_rootdir(), "data/labels_v1.csv")
-    labels_v1 = pd.read_csv(dataset_path)
-    labels = labels_v1.final
-    y_train = labels.loc[labels != "inclassable"] == "dissident"
-    X_train = labels_v1.text.loc[labels != "inclassable"]
+    dataset = pd.concat([
+        pd.read_csv(os.path.join(get_rootdir(), "data/labels_v1.csv")),
+        pd.read_csv(os.path.join(get_rootdir(), "data/labels_v2.csv"))
+    ])
+    dataset = dataset.loc[(dataset.moindze != "inclassable") &
+                          (dataset.amir != "inclassable") &
+                          (dataset.charles != "inclassable")]
+    y = dataset.final.apply(lambda x: x == "dissident")
+    X = dataset.text
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42)
 
-    return X_train, X_train, y_train, y_train
+    logging.info(
+        f"Split dataset\n"
+        f" - train: {len(X_train)} samples ({y_train.mean()*100:.1f}% pos)"
+        "\n"
+        f" - test: {len(X_test)} examples ({y_test.mean()*100:.1f}% pos)"
+    )
+
+    return X_train, X_test, y_train, y_test
