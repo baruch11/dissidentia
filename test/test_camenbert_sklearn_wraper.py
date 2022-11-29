3f632279e5030a7e4f7b41fb32d1fac2b06932f3
CHARLES-HENRI PRAT <charprat@yahoo.fr>
2022-11-29 22:33:35 +0100

    [domain] special saving method for huggingface models

--- test/test_camenbert_sklearn_wraper.py
+++ test/test_camenbert_sklearn_wraper.py
@@ -1,20 +1,37 @@
 """test Bert type model with sklearn wrapping"""
 
+import numpy as np
 from dissidentia.infrastructure.dataset import get_train_test_split
 from dissidentia.domain.sklearn_bert_wrapper import BertTypeClassifier
 
+import tempfile
 
 def camembert_sklearn_wrapper():
     """ launch with 'pytest -o python_functions="camembert_*" -s'
     """
 
     x_train, x_test, y_train, y_test = get_train_test_split(from_doccano=False)
+    x_train = x_train.iloc[:32]
+    y_train = y_train.iloc[:32]
+    x_test = x_test.iloc[:32]
+    y_test = y_test.iloc[:32]
 
     x_val = (x_test, y_test)
     model = BertTypeClassifier(val_dataset=x_val,
                                num_train_epochs=1, freezing=True)
     model.fit(x_train, y_train)
+
     y_pred = model.predict(x_test)
     y_prob = model.predict_proba(x_test)
 
     print(f"test different scores: {model.evaluate()}")
+
+    # test save / load
+    with tempfile.TemporaryDirectory() as tmpdir:
+        model.save(tmpdir)
+
+        model2 = BertTypeClassifier.load(tmpdir)
+        y_proba2 = model2.predict_proba(x_test)
+
+        assert np.max(np.abs(y_prob-y_proba2)) < 1e-6
+
